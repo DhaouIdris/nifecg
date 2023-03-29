e1464071f6adf2225b6a22ac68cd01b9a1629f8b
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2023-03-29 19:15:01 +0200

    update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -18,7 +18,7 @@ class PreprocessingBlock(Block):
 				treated signal
 			ts : ndarray
 				treated signal timestamps
-		"""
+		
 
 		# assuming the sampling frequency is constant
 		fs = 1/(ts[1] - ts[0])
@@ -39,5 +39,17 @@ class PreprocessingBlock(Block):
 		sig = signal.filtfilt(b,a,sig, axis=0)
 		sig = signal.filtfilt(bs1,as1,sig, axis=0)
 		sig = signal.filtfilt(bs2,as2,sig, axis=0)
-		
+		"""
+		fe=1000
+		fc1=5
+		fc2=15
+		nuc1 = fc1 / fe
+    		nuc2 = fc2 / fe
+    		ordre = 4
+     		Rp = 1.0
+    		Rs = 40.0
+		[b_pb, a_pb] = ellip(ordre, Rp, Rs, [2*nuc1, 2*nuc2], 'bandpass')
+	
+    		# freqz(b_pb,a_pb,10000,fe)
+		sig = filtfilt(b_pb, a_pb, sig)
 		return sig, ts
